01014cce724ccb172ad724b607c6e19898073b4e
Omid Ashtari <omidashtari@Omids-Mac-mini.local>
2020-07-14 19:02:58 -0700

    added new table to database, added related seeds to table

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     connection: {
       host: "localhost",
       user: "postgres",
-      password: "test",
+      password: "gunner",
       database: "quick_street_be",
     },
     // connection:'postgres://localhost/<examples>',
