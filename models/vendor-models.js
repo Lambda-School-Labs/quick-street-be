660b7d4995710fc9a0280a569a883e6057eeb042
Omid Ashtari <omidashtari@Omids-Mac-mini-2.local>
2020-08-02 19:10:44 -0700

    fixed model and route to allow a vendor to upload a banner image

--- models/vendor-models.js
+++ models/vendor-models.js
@@ -101,6 +101,12 @@ function addVendorPosts(data) {
 
 function addVendorBanner(vendor_id, image_data) {
   return db("vendors")
-    .where({ id: vendor_id })
-    .update({ public_id: image_data });
+    .where({ "users_id": vendor_id })
+    .update({ "public_id": image_data });
 }
+
+// function addProductImage(product_id, image_data) {
+//   return db("products")
+//   .where({"id" : product_id})
+//   .update({"public_id": image_data})
+// }
