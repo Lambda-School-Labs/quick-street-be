ed7e3dca3b192b15abd1787907b6e73b1fabee53
Omid Ashtari <omidashtari@Omids-Mac-mini-2.local>
2020-08-12 15:51:56 -0700

    maybe the % will go up?

--- routes/orders-router.spec.js
+++ routes/orders-router.spec.js
@@ -45,22 +45,36 @@ describe("GET /", () => {
 describe(" get all orders", () => {
   it("should get all the orders in the db for the vendor signed in", async () => {
 
-    return request(server)
+    let theOrders =  request(server)
     .get("/api/orders")
     .set("Authorization", `${token}`)
-    .then(res => expect(res.status).toBe(200))
-    // let theOrders = await Orders.getOrders('orders')
-    // let length = theOrders.length
-    // expect(theOrders).toHaveLength(count)
+    .then(res => {
+      console.log("ORDERS RES", res)
+      expect(res.status).toBe(200)
+      // console.log()
+      // expect(res.)
+      // let theOrders = Orders.getOrders('orders')
+      // let length = theOrders.length
+      // expect(theOrders).toHaveLength(length)
+    })
   })
-  // console.log("ORDERS RES", res)
+
+
+  // const response = await request(app).get("/");
+  //   expect(response.body).toEqual(["Elie", "Matt", "Joel", "Michael"]);
+  //   expect(response.statusCode).toBe(200);
+
+  // it("should add one more customer", async () => {
+
+  //   await Customers.add({
+  //     users_id: 20,
+  //     customer_name: "Gunner",
+  //     phone_number: "124-578-9276",
+  //     address: "hello street",
+  //     zip_code: 39872
+  //   })
+  //   const newCustomers = await db('customers')
+  //   let count = newCustomers.length;
+  //   expect(newCustomers).toHaveLength(count)
+  // })
 })
-
-
-// describe("GET /", () => {
-//   it("should return 404 restricted address not logged in", () => {
-//     request(server)
-//     .get("/vendors/1")
-//     .then((res) => expect(res.status).toBe(404));
-//     });
-//   });
