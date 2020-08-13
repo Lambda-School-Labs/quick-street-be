314567e9d0df5a1c9239afee339266a63ce274cb
Omid Ashtari <omidashtari@Omids-Mac-mini-2.local>
2020-08-12 20:12:40 -0700

    MOAR tests

--- routes/products-router.spec.js
+++ routes/products-router.spec.js
@@ -3,6 +3,24 @@ const ProductsRouter = require('./products-router');
 const server = require('../api/server.js');
 const request = require('supertest');
 
+let token;
+
+beforeAll((done)=>{
+  request(server)
+  .post("/api/auth/login")
+  .send({
+    email: "test4@test.com",
+    password: "test123"
+  })
+  .end((err, res) => {
+    // console.log("RES", res.body.token)
+    token = res.body.token;
+    done();
+  })
+})
+
+describe("products routers", () => {
+
 describe("GET /", () => {
     it("is using right testing environment", () => {
       expect(process.env.NODE_ENV).toBe("testing");
@@ -10,18 +28,35 @@ describe("GET /", () => {
   });
 
 
-  describe("GET /", () => {
+describe("GET /", () => {
+  it("should return 404 restricted address not logged in", () => {
+      request(server)
+      .get("/products")
+      .then((res) => expect(res.status).toBe(404));
+      });
+  });
+
+describe("GET /", () => {
     it("should return 404 restricted address not logged in", () => {
         request(server)
-        .get("/products")
+        .get("/products/1")
         .then((res) => expect(res.status).toBe(404));
         });
     });
 
-    describe("GET /", () => {
-        it("should return 404 restricted address not logged in", () => {
-            request(server)
-            .get("/products/1")
-            .then((res) => expect(res.status).toBe(404));
-            });
-        });
+describe("POST a product", () => {
+  it("should post a product", async () => {
+    await request(server)
+    .post("/api/products")
+    .set("Authorization", `${token}`)
+    .send({
+      vendor_id: 1,
+      name: "TEST",
+      price: 6.3,
+    })
+    let allProducts = await db('products');
+    let allCount = allProducts.length
+    expect(allProducts.length).toBe(allCount)
+  })
+})
+  })
