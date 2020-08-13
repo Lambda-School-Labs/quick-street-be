5b09df0fa97dc19c424df42434aac2f3b9df3a91
Omid Ashtari <oashtari@gmail.com>
2020-08-13 06:24:51 -0700

    blah blah tests

--- models/vendor-models.spec.js
+++ models/vendor-models.spec.js
@@ -42,6 +42,23 @@ describe("GET vendor posts by id", () => {
     });
   });
 
+describe("GET vendor by id", () => {
+it("gets vendor by id", async () => {
+    const aVenById = await Vendors.findByVendorId(2);
+    console.log("ONE VENDOR", aVenById)
+    expect(aVenById[0].zipcode).toBe(94107);
+//   expect(res.description).toBe("time to get high kitty");
+});
+});
+
+describe("GET vendor posts", () => {
+    it("gets vendor posts", async () => {
+        const vendorPosts = await Vendors.findVendorPosts(2);
+        console.log(" VENDOR POSTS", vendorPosts)
+        expect(vendorPosts[0].id).toBe(1);
+    //   expect(res.description).toBe("time to get high kitty");
+    });
+    });
 
 describe("GET vendor product by customer", () => {
     it("gets vendor products by a customer", async () => {
@@ -51,6 +68,23 @@ describe("GET vendor product by customer", () => {
     //   expect(res.description).toBe("time to get high kitty");
     });
   });
+
+
+describe("ADD a vendor post", () => {
+it("should add one more post for a vendor", async () => {
+
+    await Vendors.addVendorPosts({
+        vendors_id: 3,
+        title: "TEST POST",
+        description: "test meow",
+      })
+    const newPosts = await db('posts')
+    let postsLength = newPosts.length;
+    expect(newPosts).toHaveLength(postsLength)
+})
+})
+
+
 })
 
 
