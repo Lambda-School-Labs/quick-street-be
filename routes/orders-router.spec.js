195645f66a5241bf2daa591baaead6d99f443dbe
Omid Ashtari <omidashtari@Omids-Mac-mini-2.local>
2020-08-12 18:16:54 -0700

    uncommented the three test files

--- routes/orders-router.spec.js
+++ routes/orders-router.spec.js
@@ -1,64 +1,64 @@
-// const db = require('../data/db-config.js');
-// const Orders = require('./orders-router');
-// const server = require('../api/server.js');
-// const request = require('supertest');
+const db = require('../data/db-config.js');
+const Orders = require('./orders-router');
+const server = require('../api/server.js');
+const request = require('supertest');
 
-// let token;
+let token;
 
-// beforeAll((done)=>{
-//   request(server)
-//   .post("/api/auth/login")
-//   .send({
-//     email: "test4@test.com",
-//     password: "test123"
-//   })
-//   .end((err, res) => {
-//     token = res.body.token;
-//     done();
-//   })
-// })
+beforeAll((done)=>{
+  request(server)
+  .post("/api/auth/login")
+  .send({
+    email: "test4@test.com",
+    password: "test123"
+  })
+  .end((err, res) => {
+    token = res.body.token;
+    done();
+  })
+})
 
-// describe("GET /", () => {
-//     it("is using right testing environment", () => {
-//       expect(process.env.NODE_ENV).toBe("testing");
-//     });
-//   });
+describe("GET /", () => {
+    it("is using right testing environment", () => {
+      expect(process.env.NODE_ENV).toBe("testing");
+    });
+  });
 
 
-// describe("GET /", () => {
-// it("should return 404 restricted address not logged in", () => {
-//     request(server)
-//     .get("/orders")
-//     .then((res) => expect(res.status).toBe(404));
-//     });
-// });
+describe("GET /", () => {
+it("should return 404 restricted address not logged in", () => {
+    request(server)
+    .get("/orders")
+    .then((res) => expect(res.status).toBe(404));
+    });
+});
 
-// describe("GET /", () => {
-//     it("should return 404 restricted address not logged in", () => {
-//         request(server)
-//         .get("/orders/1")
-//         .then((res) => expect(res.status).toBe(404));
-//         });
-//     });
+describe("GET /", () => {
+    it("should return 404 restricted address not logged in", () => {
+        request(server)
+        .get("/orders/1")
+        .then((res) => expect(res.status).toBe(404));
+        });
+    });
 
-// describe(" get all orders", () => {
-//   it("should get all the orders in the db for the vendor signed in", async () => {
+describe(" get all orders", () => {
+  it("should get all the orders in the db for the vendor signed in", async () => {
 
-//     // let theOrders =  
-//     request(server)
-//     .get("/api/orders")
-//     .set("Authorization", `${token}`)
-//     .then(res => {
-//       // console.log("ORDERS RES", res.body)
-//       let theOrders = res.body;
-//       // console.log("THE ORDERS", theOrders)
-//       // let length = theOrders.length
-//       expect(res.status).toBe(200)
-//       // console.log("LENGTH", length)
-//       expect(theOrders).toHaveLength(6)
+    // let theOrders =  
+    request(server)
+    .get("/api/orders")
+    .set("Authorization", `${token}`)
+    .then(res => {
+      // console.log("ORDERS RES", res.body)
+      let theOrders = res.body;
+      // console.log("THE ORDERS", theOrders)
+      // let length = theOrders.length
+      expect(res.status).toBe(200)
+      // console.log("LENGTH", length)
+      expect(theOrders).toHaveLength(6)
       
-//       // let theOrders = Orders.getOrders('orders')
-//     })
-//   })
+      // let theOrders = Orders.getOrders('orders')
+    })
+  })
 
-// })
+})
--- routes/vendor-router.spec.js
+++ routes/vendor-router.spec.js
@@ -1,71 +1,71 @@
-// const db = require('../data/db-config.js');
-// const VendorsRouter = require('./vendor-router');
-// const server = require('../api/server.js');
-// const request = require('supertest');
+const db = require('../data/db-config.js');
+const VendorsRouter = require('./vendor-router');
+const server = require('../api/server.js');
+const request = require('supertest');
 
-// let token;
+let token;
 
-// beforeAll((done)=>{
-//   request(server)
-//   .post("/api/auth/login")
-//   .send({
-//     email: "test4@test.com",
-//     password: "test123"
-//   })
-//   .end((err, res) => {
-//     // console.log("RES", res.body.token)
-//     token = res.body.token;
-//     done();
-//   })
-// })
+beforeAll((done)=>{
+  request(server)
+  .post("/api/auth/login")
+  .send({
+    email: "test4@test.com",
+    password: "test123"
+  })
+  .end((err, res) => {
+    // console.log("RES", res.body.token)
+    token = res.body.token;
+    done();
+  })
+})
 
-// describe("vendor routers", () => {
+describe("vendor routers", () => {
 
-//   describe("GET /", () => {
-//     it("is using right testing environment", () => {
-//       expect(process.env.NODE_ENV).toBe("testing");
-//     });
-//   });
+  describe("GET /", () => {
+    it("is using right testing environment", () => {
+      expect(process.env.NODE_ENV).toBe("testing");
+    });
+  });
 
 
-//   describe("GET /", () => {
-//     it("should return 404 restricted address not logged in", () => {
-//       request(server).get("/vendors")
-//       .then((res) => expect(res.status).toBe(404));
-//       });
-//     });
+  describe("GET /", () => {
+    it("should return 404 restricted address not logged in", () => {
+      request(server).get("/vendors")
+      .then((res) => expect(res.status).toBe(404));
+      });
+    });
 
-//   describe("GET /", () => {
-//     it("should return 404 restricted address not logged in", () => {
-//       request(server)
-//       .get("/vendors/1")
-//       .then((res) => expect(res.status).toBe(404));
-//       });
-//     });
+  describe("GET /", () => {
+    it("should return 404 restricted address not logged in", () => {
+      request(server)
+      .get("/vendors/1")
+      .then((res) => expect(res.status).toBe(404));
+      });
+    });
 
-//   describe(" /me", () => {
-//     it("get logged in vendor's info", async () => {
-//       return request(server)
-//       .get("/api/vendors/me")
-//       .set('Authorization', `${token}`)
-//       .then(res => {
-//         expect(res.status).toBe(201)
-//       })
+  describe(" /me", () => {
+    it("get logged in vendor's info", async () => {
+      return request(server)
+      .get("/api/vendors/me")
+      .set('Authorization', `${token}`)
+      .then(res => {
+        expect(res.status).toBe(201)
+      })
   
-//     })
-//   })
+    })
+  })
 
-//   describe("/me/products", () => {
-//     it("should get all of the products of the vendor", async () => {
-//       return request(server)
-//       .get("/api/vendors/me/products")
-//       .set("Authorization", `${token}`)
-//       .then(res => {
-//         // expect(res.status).toBe(200)
-//       })
-//     })
-//   })
-// })
+  describe("/me/products", () => {
+    it("should get all of the products of the vendor", async () => {
+      return request(server)
+      .get("/api/vendors/me/products")
+      .set("Authorization", `${token}`)
+      .then(res => {
+        // expect(res.status).toBe(200)
+      })
+    })
+  })
+})
 
 
 
